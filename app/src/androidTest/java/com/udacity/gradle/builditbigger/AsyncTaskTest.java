035a4f80c9e9539a7e70b4920bf7abd85a75208f
ahmed <ahmed.alaa.cis@gmail.com>
2017-11-29 19:55:19 +0200

    fix AsyncTaskTest

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -9,26 +9,37 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class AsyncTaskTest {
-
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    CountDownLatch countDownLatch;
+    String joke = null;
 
     @Test
     public void asyncTaskTest() {
-
+        countDownLatch = new CountDownLatch(1);
         JokeAsyncTask jokeAsyncTask = new JokeAsyncTask(new SendInterface() {
             @Override
             public void onPostExecute(String s) {
-                assertNotNull(s);
-                assertFalse(s.isEmpty());
+                joke = s;
             }
         }, mActivityTestRule.getActivity());
         jokeAsyncTask.execute();
+        try {
+            countDownLatch.await(10, TimeUnit.SECONDS);
+            assertNotNull(joke);
+            assertFalse(joke.isEmpty());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
     }
 }
